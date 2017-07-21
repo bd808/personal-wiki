c1da19df68ccf719932599e4927763cdf014baf7
Bryan Davis <bd808@wikimedia.org>
2017-07-20 22:43:38 -0600

    Combine both scripts
    Combine the functionality of both scripts into one. The updated
    project-dsh-generator.py no longer supports generating an 'all-instances'
    list. Instead the `--all` option will generate a separate list for each
    project known to openstack-browser. It will also refuse to generate
    a list for a project with no instances.

--- project-dsh-generator.py
+++ project-dsh-generator.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 """
 Simple script that generates hosts files usable with dsh/pssh
-for runnign administrative actions on all hosts in a particular
-labs project. Salt is just way too unreliable to be useful
-for this purpose.
+for running administrative actions on all hosts in a particular
+labs project.
 
 Hits the openstack-browser API.
 
@@ -13,25 +12,87 @@ You can execute commands via pssh like:
 
 This sets parallelism to 4, tweak as necessary.
 """
-import json
-import sys
 from urllib.request import urlopen
+import argparse
+import sys
 
 OSB = 'https://tools.wmflabs.org/openstack-browser'
 
-project_spec = sys.argv[1]
+# Maps prefixes to hostgroup names
+classifier = {
+    'tools': {
+        '': 'all',
+        'tools-bastion-': 'bastion',
+        'tools-checker-': 'checker',
+        'tools-cron-': 'cron',
+        'tools-docker-': 'docker',
+        'tools-elastic-': 'elastic',
+        'tools-exec-': 'exec-all',
+        'tools-flannel-': 'flannel',
+        'tools-k8s-': 'k8s',
+        'tools-mail': 'mail',
+        'tools-master': 'master',
+        'tools-proxy-': 'proxy',
+        'tools-redis-': 'redis',
+        'tools-services-': 'services',
+        'tools-static-': 'static',
+        'tools-webgrid-': 'webgrid-all',
+        'tools-webgrid-generic': 'webgrid-generic',
+        'tools-webgrid-lighttpd-': 'webgrid-lighttpd-all',
+        'tools-webgrid-lighttpd-14': 'webgrid-lighttpd-trusty',
+        'tools-worker-': 'worker',
+    },
+}
 
-if project_spec == 'all-instances':
-    projects_url = '{}/api/projects.txt'.format(OSB)
-    projects = urlopen(projects_url).read().decode('utf-8').split('\n')
-else:
-    projects = [project_spec]
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
 
-instances = []
-for project_name in projects:
-    api_url = '{}/api/dsh/project/{}'.format(OSB, project_name)
-    hosts = urlopen(api_url).read().decode('utf-8').split('\n')
-    instances.extend(hosts)
 
-with open(project_spec, 'w') as f:
-    f.write("\n".join(instances))
+def get_list(url):
+    body = urlopen(url).read().decode('utf-8').strip()
+    if not body:
+        return []
+    return body.split('\n')
+
+
+def main():
+    parser = argparse.ArgumentParser(description='DSH hostfile generator')
+    parser.add_argument(
+        'project', metavar='PROJECT', nargs='*',
+        help='Cloud VPS project')
+    parser.add_argument(
+        '--all', action='store_true',
+        help='Generate files for all projects')
+    args = parser.parse_args()
+
+    if args.all:
+        projects_url = '{}/api/projects.txt'.format(OSB)
+        projects = get_list(projects_url)
+    elif not args.project:
+        parser.error('No project names provided and `--all` not used.')
+    else:
+        projects = args.project
+
+    for project in projects:
+        eprint('Fetching hosts for project {}'.format(project))
+        api_url = '{}/api/dsh/project/{}'.format(OSB, project)
+        hosts = get_list(api_url)
+
+        if not hosts:
+            eprint(' - No hosts in project {}'.format(project))
+            continue
+
+        if project in classifier:
+            for prefix, group in classifier[project].items():
+                with open('{}-{}'.format(project, group), 'w') as f:
+                    f.write("\n".join([
+                        host
+                        for host in hosts
+                        if host.startswith(prefix)
+                    ]))
+        else:
+            with open(project, 'w') as f:
+                f.write("\n".join(hosts))
+
+
+main()
