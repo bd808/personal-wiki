23ef0ade4f5c8e90b88f983a51e6e82ac2b6d6b7
Bryan Davis <bd808@wikimedia.org>
2017-07-23 23:38:10 -0600

    Add a --clush output mode
    * --clush generates a yaml dump of the hostgroup tree
    * --outfile provides control of where the files are output

--- project-dsh-generator.py
+++ project-dsh-generator.py
@@ -14,6 +14,7 @@ This sets parallelism to 4, tweak as necessary.
 """
 from urllib.request import urlopen
 import argparse
+import contextlib
 import os
 import sys
 import yaml
@@ -33,6 +34,28 @@ def get_list(url):
     return body.split('\n')
 
 
+@contextlib.contextmanager
+def _open(fname=None):
+    """Based on https://stackoverflow.com/a/17603000/8171"""
+    if fname and fname != '-':
+        fh = open(fname, 'w')
+    else:
+        fh = sys.stdout
+    try:
+        yield fh
+    finally:
+        if fh is not sys.stdout:
+            fh.close()
+
+
+def _outfile(base, fname):
+    if base == '-':
+        return base
+    if os.path.isfile(base):
+        return base
+    return os.path.join(base, fname)
+
+
 def main():
     parser = argparse.ArgumentParser(description='DSH hostfile generator')
     parser.add_argument(
@@ -46,6 +69,13 @@ def main():
             'classifiers.yaml'),
         help='Generate files for all projects')
     parser.add_argument(
+        '--output',
+        default=os.getcwd(),
+        help='Directory or file to output results to.')
+    parser.add_argument(
+        '--clush', action='store_true',
+        help='Generate clush hostgroup mapping file instead of dsh files.')
+    parser.add_argument(
         'project', metavar='PROJECT', nargs='*',
         help='Cloud VPS project')
     args = parser.parse_args()
@@ -58,11 +88,23 @@ def main():
     else:
         projects = args.project
 
+    # Validate output file/directory
+    if args.output == '-':
+        outpath = '-'
+    else:
+        outpath = os.path.abspath(args.output)
+        if len(projects) > 1 and not os.path.isdir(outpath):
+            parser.error(
+                'Output location is not a directory and multiple'
+                'projects are selected')
+
     try:
         with open(args.classifiers, 'r') as f:
             classifiers = yaml.safe_load(f.read())
     except IOError as e:
         classifiers = {}
+
+    hostgroups = {}
 
     for project in projects:
         eprint('Fetching hosts for project {}'.format(project))
@@ -75,15 +117,22 @@ def main():
 
         if project in classifiers:
             for prefix, group in classifiers[project].items():
-                with open('{}-{}'.format(project, group), 'w') as f:
-                    f.write("\n".join([
-                        host
-                        for host in hosts
-                        if host.startswith(prefix)
-                    ]))
+                key = '{}-{}'.format(project, group)
+                hostgroups[key] = [
+                    host
+                    for host in hosts
+                    if host.startswith(prefix)
+                ]
         else:
-            with open(project, 'w') as f:
-                f.write("\n".join(hosts))
+            hostgroups[project] = hosts
+
+    if args.clush:
+        with _open(_outfile(args.output, 'hostgroups.yaml')) as f:
+            f.write(yaml.safe_dump(hostgroups, default_flow_style=False))
+    else:
+        for name, data in hostgroups.items():
+            with _open(_outfile(args.output, name)) as f:
+                f.write("\n".join(data))
 
 
 main()
